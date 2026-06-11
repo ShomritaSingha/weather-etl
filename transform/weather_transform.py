258d99340eb815a86ac64fa6b87aa8e34371cc42
Shomrita Singha <shomritasingha@Shomritas-MacBook-Air.local>
2026-06-11 21:01:18 +0530

    eddited the r transformation layer

--- transform/weather_transform.py
+++ transform/weather_transform.py
@@ -1,19 +1,23 @@
 
 
 
-def tranformed_weather_data(raw_data):
-    transfomred_data =  {
-        "location": (f"{raw_data['latitude']}, {raw_data['longitude']}"),
-        "temperature" : (f"Temparature: {raw_data['current']['temperature_2m'] } {raw_data['current_units']['temperature_2m']}"),
-        "time" : (f" time :{raw_data['current'][ 'time']}"),
-        "wind_speed" : (f"wind_speed: {raw_data['current']['wind_speed_10m'] } {raw_data['current_units']['wind_speed_10m']}")
+def transformed_weather_data(raw_data):
+    transformed_data = {
+    "latitude": raw_data["latitude"],
+    "longitude": raw_data["longitude"],
+    "temperature": raw_data["current"]["temperature_2m"],
+    "temperature_unit": raw_data["current_units"]["temperature_2m"],
+    "time": raw_data["current"]["time"],
+    "wind_speed": raw_data["current"]["wind_speed_10m"],
+    "wind_speed_unit": raw_data["current_units"]["wind_speed_10m"]
+}
         
        
-    }
-    print("works")
+    
+    
      
     
-    return transfomred_data
+    return transformed_data
 
 
         
